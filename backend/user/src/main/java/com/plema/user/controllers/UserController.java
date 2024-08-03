51ea6c62a40d74d46015298b0fb0acbc65d241f5
Oleg Plema <olegplema01@gmail.com>
2024-08-03 12:50:10 +0300

    Initial config

---
+++ backend/user/src/main/java/com/plema/user/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.plema.user.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.plema.user.services.interfaces.UserService;
+
+@RestController
+class UserController {
+
+}
---
+++ backend/user/src/main/java/com/plema/user/entities/User.java
@@ -0,0 +1,23 @@
+package com.plema.user.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String pubKey;
+
+    private String username;
+
+    private String email;
+}
